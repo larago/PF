d9e5e95e69f525bcb49ca2d2bb14b01b75021729
larago <bingeye@foxmail.com>
2016-12-03 21:33:06 +0800

    more

---
+++ 10.py
@@ -0,0 +1,25 @@
+bands = [{'name': 'sunset rubdown', 'country': 'UK', 'active': False},
+         {'name': 'women', 'country': 'Germany', 'active': False},
+         {'name': 'a silver mt. zion', 'country': 'Spain', 'active': True}]
+
+def assoc(_d, key, value):
+    from copy import deepcopy
+    d = deepcopy(_d)
+    d[key] = value
+    return d
+
+def set_canada_as_country(band):
+    return assoc(band, 'country', 'Canada')
+
+def strip_punctuation_from_name(band):
+    return assoc(band, 'name', band['name'].replace('.', ''))
+
+def capitalize_names(band):
+    return assoc(band, 'name', band['name'].title())
+
+def pipeline_each(data, fns):
+    return reduce(lambda a, x: map(x, a), fns, data)
+
+print pipeline_each(bands, [set_canada_as_country,
+                            strip_punctuation_from_name,
+                            capitalize_names])
---
+++ 9.py
@@ -0,0 +1,13 @@
+bands = [{'name': 'sunset rubdown', 'country': 'UK', 'active': False},
+         {'name': 'women', 'country': 'Germany', 'active': False},
+         {'name': 'a silver mt. zion', 'country': 'Spain', 'active': True}]
+
+def format_bands(bands):
+    for band in bands:
+        band['country'] = 'Canada'
+        band['name'] = band['name'].replace('.', '')
+        band['name'] = band['name'].title()
+
+format_bands(bands)
+
+print bands
